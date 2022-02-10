cda2d150523fecc1a855f1562837004ad835d480
Courdevelops <courdevelops@gmail.com>
2022-02-10 16:16:26 -0500

    mongoose

--- server.js
+++ server.js
@@ -22,35 +22,90 @@ app.listen(3000)
 
 //MONGODB
 
-// load module driver
-const mongodb = require('mongodb');
+// // load module driver
+// const mongodb = require('mongodb');
 
-// create a client instance using mongo db driver
-const MongoClient = mongodb.MongoClient;
+// // create a client instance using mongo db driver
+// const MongoClient = mongodb.MongoClient;
 
-// pulling the address of the server needed connection
-    // uses mangodb protocol to communicate w/ server
-const connectionStr = 'mongodb://localhost:27017';
+// // pulling the address of the server needed connection
+//     // uses mangodb protocol to communicate w/ server
+// const connectionStr = 'mongodb://localhost:27017';
 
-// make a new client new instance to connect to db server
-const client = new MongoClient(connectionStr);
+// // make a new client new instance to connect to db server
+// const client = new MongoClient(connectionStr);
 
-// using client connect to server
-// takes a function
-client.connect(function(err) {
+// // using client connect to server
+// // takes a function
+// client.connect(function(err) {
     
-    // error handle
-    if (err) { throw new Error(err) }
+//     // error handle
+//     if (err) { throw new Error(err) }
 
-    // select the particular data base (container for the tables goes here)
-        //in this case it's nbadb where the 'teams' collection is housed
-    const db = client.db('nbadb');
+//     // select the particular data base (container for the tables goes here)
+//         //in this case it's nbadb where the 'teams' collection is housed
+//     const db = client.db('nbadb');
 
-    // select the particular collection in the data base
-    const teams = db.collection('teams');
+//     // select the particular collection in the data base
+//     const teams = db.collection('teams');
 
-    teams.find({}).toArray((err, allData) => {
-        console.log(allData)
-    })
+//     teams.find({}).toArray((err, allData) => {
+//         console.log(allData)
+//     })
 
+// })
+
+//MONGOOSE
+
+// load the mongoose module
+const mongoose = require('mongoose');
+
+// connect to the database(BASIC)
+// mongoose.connect('mongodb://localhost:27017/nba2db');
+
+// " in a more advanced way
+    //create connectionStr
+const connectionStr = "mongodb://localhost:27017/nba2db"
+// connect to the database
+mongoose.connect(connectionStr).then(() => {
+    console.log('Database Connected')
 })
+
+// create the schema
+    //for more teams
+let teamSchema = mongoose.Schema({
+    name: String,
+    location: String,
+    yearformed: String,
+})
+
+// creating the model a controller for collection inside the database
+let TeamModel = mongoose.model("Team", teamSchema)
+
+//instantiate the model
+let team5 = new TeamModel({name: "trailblazers", location: "portland", yearformed: "1979"})
+let team6 = new TeamModel({name: "pistons", location: "detroit", yearformed: "1941"})
+let team7 = new TeamModel({name: "lakers", location: "los angeles", yearformed: "1965"})
+
+
+//NOW save the data (instantiations)
+team5.save(function(err, team) {
+    if (err) { return console.error(err) }
+    console.log(team.name + " saved to nba2 teams")
+});
+
+team6.save(function(err, team) {
+    if (err) { return console.error(err) }
+    console.log(team.name + " saved to nba2 teams")
+});
+
+team7.save(function(err, team) {
+    if (err) { return console.error(err) }
+    console.log(team.name + " saved to nba2 teams")
+});
+
+// use model to find and read data
+TeamModel.find({ name: "pistons" }).then(teams => {
+    console.log(teams)
+})
+
